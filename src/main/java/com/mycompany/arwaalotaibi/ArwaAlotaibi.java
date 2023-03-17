fda791095bf5ad208ceba03a349e5bd03b6648d0
Arwa Alotaibi <armo03730@gmail.com>
2023-03-17 14:07:07 +0300

    First commit!

---
+++ src/main/java/com/mycompany/arwaalotaibi/ArwaAlotaibi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arwaalotaibi;
+
+/**
+ *
+ * @author armo0
+ */
+public class ArwaAlotaibi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
